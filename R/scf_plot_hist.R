c38b2fc20e8683f8296d9550e7ac43faab06a57b
Joseph N. Cohen <josephncohen@gmail.com>
2026-05-03 10:12:21 -0400

    fix bug in histogram function

--- R/scf_plot_hist.R
+++ R/scf_plot_hist.R
@@ -69,7 +69,8 @@ scf_plot_hist <- function(design, variable,
   breaks <- seq(rng[1], rng[2], length.out = bins + 1)
   
   # Clamp and bin within each implicate
-  design <- scf_update_by_implicate(design, function(df) {
+  design <- scf_update_by_implicate(design, function(d) {
+    df <- d$variables
     x <- df[[varname]]
     x <- pmin(pmax(x, rng[1]), rng[2])
     df$.binvar <- cut(x, breaks = breaks, include.lowest = TRUE, right = TRUE)
